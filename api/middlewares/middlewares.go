54b2a3491fc7e0d6b02ff619d62331a90c9c3c0d
raazeshp96 <raazeshp96@gmail.com>
2021-02-11 12:42:31 +0545

    created controller for the api

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/victorsteven/fullstack/api/auth"
-	"github.com/victorsteven/fullstack/api/responses"
+	"github.com/RaazeshP96/CRUD-RESTful-API-with-Go/api/auth"
+	"github.com/RaazeshP96/CRUD-RESTful-API-with-Go/api/responses"
 )
 
 func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
